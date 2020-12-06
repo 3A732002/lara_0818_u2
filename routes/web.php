06a13d0f8481fcbe9475253f4559f23995e91a22
ma yu cen <sheehan010507@hotmail.com>
2020-12-06 21:01:52 +0800

    設定route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HelloController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('home',[HomeController::class,'index']);
 
+Route::get('hello/{name?}',[HelloController::class,'index'])->name('Hello.index');
